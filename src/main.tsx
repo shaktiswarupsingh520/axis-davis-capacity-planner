2b8c0295dda38f64784740c882baa26a9d25ade6
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-19 05:12:29 +0530

    Use production PDF renderer and chart UX fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import EnhancedApp from './EnhancedApp';
-import { installPdfReportOverride } from './reportOverride';
 import { installChartUxFixes } from './chartUxFixes';
+import { installProductionPdf } from './productionPdf';
 import './index.css';
 import './realApp.css';
 import './enhanced.css';
 import './branding.css';
 
-installPdfReportOverride();
 installChartUxFixes();
+installProductionPdf();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
