a17d98cd7383aee960489df7d65618969eb565a8
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-19 05:11:20 +0530

    Install date-aware chart UX fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import EnhancedApp from './EnhancedApp';
 import { installPdfReportOverride } from './reportOverride';
+import { installChartUxFixes } from './chartUxFixes';
 import './index.css';
 import './realApp.css';
 import './enhanced.css';
 import './branding.css';
 
 installPdfReportOverride();
+installChartUxFixes();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
