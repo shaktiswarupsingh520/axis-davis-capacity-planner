fb2bbbdccd11f3405fda008fcd366a38cb3bb14f
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-19 04:54:52 +0530

    Enable production report renderer with interactive chart enhancements

--- src/EnhancedApp.tsx
+++ src/EnhancedApp.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
 import RealAppV2 from './RealAppV2';
 import { installUiEnhancements } from './uiEnhancements';
+import { installPdfReportOverride } from './reportOverride';
 
 export default function EnhancedApp() {
   useEffect(() => {
     installUiEnhancements();
+    installPdfReportOverride();
   }, []);
   return <RealAppV2 />;
 }
