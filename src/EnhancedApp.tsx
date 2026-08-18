9ce600657d69a332b4c5508872625f4576a91134
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-18 21:01:07 +0530

    Fix runtime hang by removing DOM mutation observer enhancements

--- src/EnhancedApp.tsx
+++ src/EnhancedApp.tsx
@@ -1,10 +1,5 @@
-import { useEffect } from 'react';
 import RealAppV2 from './RealAppV2';
-import { installUiEnhancements } from './uiEnhancements';
 
 export default function EnhancedApp() {
-  useEffect(() => {
-    installUiEnhancements();
-  }, []);
   return <RealAppV2 />;
 }
