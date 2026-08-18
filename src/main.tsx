f47301062d798c969eeab1f3b1e747a0d6161805
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-18 17:43:26 +0530

    Use enhanced PDF export and branding styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import EnhancedApp from './EnhancedApp';
+import { installPdfReportOverride } from './reportOverride';
 import './index.css';
 import './realApp.css';
 import './enhanced.css';
 import './branding.css';
+
+installPdfReportOverride();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
