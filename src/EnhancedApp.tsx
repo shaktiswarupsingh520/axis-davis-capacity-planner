f9984909f43487421609366a84af85f38f9cfba8
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-18 21:28:09 +0530

    Enable safe event-driven chart enhancements

--- src/EnhancedApp.tsx
+++ src/EnhancedApp.tsx
@@ -1,5 +1,10 @@
+import { useEffect } from 'react';
 import RealAppV2 from './RealAppV2';
+import { installUiEnhancements } from './uiEnhancements';
 
 export default function EnhancedApp() {
+  useEffect(() => {
+    installUiEnhancements();
+  }, []);
   return <RealAppV2 />;
 }
