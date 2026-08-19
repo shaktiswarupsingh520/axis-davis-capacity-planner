2f3bf83a4166cd60cdaec830b9a807feb4bf13f4
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-19 05:31:17 +0530

    Remove conflicting legacy chart and PDF installers

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import EnhancedApp from './EnhancedApp';
-import { installChartUxFixes } from './chartUxFixes';
-import { installProductionPdf } from './productionPdf';
 import './index.css';
 import './realApp.css';
 import './enhanced.css';
 import './branding.css';
-
-installChartUxFixes();
-installProductionPdf();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
