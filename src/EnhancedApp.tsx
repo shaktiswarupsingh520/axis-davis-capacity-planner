cc00bc9f1fec3784b9c5822dd55578863f4a23d9
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-20 09:41:59 +0530

    feat: apply Dynatrace sidebar branding patch

--- src/EnhancedApp.tsx
+++ src/EnhancedApp.tsx
@@ -3,12 +3,14 @@ import RealAppV2 from './RealAppV2';
 import { installUiFixesV4 } from './uiFixesV4';
 import { installSimulationProjectionV4 } from './simulationProjectionV4';
 import { installProductionPdfV4 } from './productionPdfV4';
+import { installBrandingPatchV41 } from './brandingPatchV41';
 
 export default function EnhancedApp() {
   useEffect(() => {
     installUiFixesV4();
     installSimulationProjectionV4();
     installProductionPdfV4();
+    installBrandingPatchV41();
   }, []);
   return <RealAppV2 />;
 }
