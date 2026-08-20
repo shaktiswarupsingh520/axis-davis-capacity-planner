ea32c6aa750cad8263b9216eb84a7c68028e81d7
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-20 16:33:07 +0530

    feat: wire segment, assist and hover fixes

--- src/EnhancedApp.tsx
+++ src/EnhancedApp.tsx
@@ -4,6 +4,9 @@ import { installUiFixesV4 } from './uiFixesV4';
 import { installSimulationProjectionV4 } from './simulationProjectionV4';
 import { installProductionPdfV4 } from './productionPdfV4';
 import { installBrandingPatchV41 } from './brandingPatchV41';
+import { installHoverMetricFixV42 } from './hoverMetricFixV42';
+import { installAssistPayloadGuardV42 } from './assistPayloadGuardV42';
+import SegmentScopeV42 from './segmentScopeV42';
 
 export default function EnhancedApp() {
   useEffect(() => {
@@ -11,6 +14,8 @@ export default function EnhancedApp() {
     installSimulationProjectionV4();
     installProductionPdfV4();
     installBrandingPatchV41();
+    installHoverMetricFixV42();
+    installAssistPayloadGuardV42();
   }, []);
-  return <RealAppV2 />;
+  return <><RealAppV2 /><SegmentScopeV42 /></>;
 }
