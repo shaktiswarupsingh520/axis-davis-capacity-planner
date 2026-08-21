4d1c7fd802984a773c84128851cf2664940a0c17
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-21 13:16:20 +0530

    feat: use v44 forecast and risk UX

--- src/EnhancedApp.tsx
+++ src/EnhancedApp.tsx
@@ -3,20 +3,18 @@ import RealAppV2 from './RealAppV2';
 import { installUiFixesV4 } from './uiFixesV4';
 import { installSimulationProjectionV4 } from './simulationProjectionV4';
 import { installProductionPdfV4 } from './productionPdfV4';
-import { installForecastEnhancementV43 } from './forecastEnhancementV43';
 import { installAiForecastEnhancementV42 } from './aiForecastEnhancementV42';
 import { installManagementZoneSearchV43 } from './managementZoneSearchV43';
-import { installOverviewRiskDrilldownV43 } from './overviewRiskDrilldownV43';
+import { installCapacityUxV44 } from './capacityUxV44';
 
 export default function EnhancedApp() {
   useEffect(() => {
     installUiFixesV4();
     installSimulationProjectionV4();
     installProductionPdfV4();
-    installForecastEnhancementV43();
     installAiForecastEnhancementV42();
     installManagementZoneSearchV43();
-    installOverviewRiskDrilldownV43();
+    installCapacityUxV44();
   }, []);
   return <RealAppV2 />;
 }
