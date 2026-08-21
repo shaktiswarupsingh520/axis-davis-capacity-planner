8ec549c167c1366148c4f5c4c13c199624eca3c7
shaktiswarupsingh520 <56478868+shaktiswarupsingh520@users.noreply.github.com>
2026-08-21 12:20:34 +0530

    feat: enable enhanced host and scope forecast view

--- src/EnhancedApp.tsx
+++ src/EnhancedApp.tsx
@@ -3,12 +3,14 @@ import RealAppV2 from './RealAppV2';
 import { installUiFixesV4 } from './uiFixesV4';
 import { installSimulationProjectionV4 } from './simulationProjectionV4';
 import { installProductionPdfV4 } from './productionPdfV4';
+import { installForecastEnhancementV42 } from './forecastEnhancementV42';
 
 export default function EnhancedApp() {
   useEffect(() => {
     installUiFixesV4();
     installSimulationProjectionV4();
     installProductionPdfV4();
+    installForecastEnhancementV42();
   }, []);
   return <RealAppV2 />;
 }
